3c74c65b2e98f165eca58845e352b03a2268083c
James A. Howard <jahoward11@gmail.com>
2019-12-09 15:09:39 -0800

    Insert tutorial-webapp1 apiUrl.

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://2224383c.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://c008d52c.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
